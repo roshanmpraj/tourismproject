547241a410374bbc9df93a95eb5863ca9a12a2b4
roshanmpraj <learnpython147@gmail.com>
2025-09-28 12:14:15 +0530

    3 data_register.py

--- model_building/data_register.py
+++ model_building/data_register.py
@@ -2,14 +2,11 @@ from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
-
-repo_id = "Roshanmpraj/Tourism"    # please create your space and repository
-
-repo_type = "dataset"
-
 # Initialize API client
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
+repo_id = "Roshanmpraj/Tourism"  # please create your space and repository
+repo_type = "dataset"
 
 # Step 1: Check if the space exists
 try:
@@ -20,16 +17,9 @@ except RepositoryNotFoundError:
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
     print(f"Space '{repo_id}' created.")
 
+# Step 2: Upload data folder to repository
 api.upload_folder(
-<<<<<<< HEAD
     folder_path="data",
-=======
-<<<<<<< HEAD
-    folder_path="data",
-=======
-    folder_path="data",
->>>>>>> 56d2eae19a1dd642ded8778766eddace54891bb0
->>>>>>> 0d7e26c74894916eadade37083f9733b68e8f8a1
     repo_id=repo_id,
     repo_type=repo_type,
 )
